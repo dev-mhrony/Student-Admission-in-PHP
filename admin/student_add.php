f04cac0f21ade713b78a787f2c9dbac9576f7669
MH RONY <78216965+dev-mhrony@users.noreply.github.com>
2023-03-04 13:53:40 +0600

    Last update - 04/03/2023

--- admin/student_add.php
+++ admin/student_add.php
@@ -1,5 +1,10 @@
 <!-- ===============================================
-	**** A COMPLETE VALIDATE FORM WITH PHP ****
+	Author Name: MH RONY.
+                        GigHub Link: https://github.com/dev-mhrony
+                        Facebook Link:https://www.facebook.com/dev.mhrony
+                        Youtube Link: https://www.youtube.com/channel/UChYhUxkwDNialcxj-OFRcDw
+                        for any PHP, Laravel, Python, Dart, Flutter work contact me at developer.mhrony@gmail.com  
+                        Visit My Website : developerrony.com
 ================================================ -->
 
 <!-- ==============  PHP begin  =================-->
